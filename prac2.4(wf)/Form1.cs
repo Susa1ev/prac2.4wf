b3b950b9e8726c6399c19ae43b4e0332c84bc0e9
Susa1ev <susalytt@gmail.com>
2021-11-30 17:44:36 +0300

    Добавьте файлы проекта.

---
+++ prac2.4(wf)/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prac2._4_wf_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Main()
+        {
+            label1.Text = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 4 - i; j >= 0; j--)
+                {
+                    label1.Text += j + "    ";
+                }
+                label1.Text +="\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Main();
+        }
+    }
+}
